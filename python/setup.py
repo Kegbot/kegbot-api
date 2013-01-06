dd68872e5e52b16c2404451f1269b417657c2489
mike wakerly <opensource@hoho.com>
2013-01-05 20:38:23 -0800

    setup: remove explicit distribute dependency; remove protobuf link.

--- python/setup.py
+++ python/setup.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
+
 """Kegbot API client library.
+
+For more information and documentation, see http://kegbot.org/docs
 """
 
-VERSION = '0.1.2'
+from setuptools import setup, find_packages
+
+VERSION = '0.1.3-pre'
 DOCLINES = __doc__.split('\n')
 SHORT_DESCRIPTION = DOCLINES[0]
 LONG_DESCRIPTION = '\n'.join(DOCLINES[2:])
 
 def setup_package():
-  from distribute_setup import use_setuptools
-  use_setuptools()
-  from setuptools import setup, find_packages
 
   setup(
       name = 'kegbot-api',
@@ -22,17 +24,11 @@ def setup_package():
       url = 'http://kegbot.org/',
       packages = find_packages(exclude=['testdata']),
       namespace_packages = ['kegbot'],
-      scripts = [
-        'distribute_setup.py',
-      ],
       install_requires = [
         'kegbot-pyutils >= 0.1.4',
         'python-gflags >= 1.8',
         'protobuf >= 2.4.1',
         'requests',
-      ],
-      dependency_links = [
-          'https://github.com/rem/python-protobuf/tarball/master#egg=protobuf-2.4.1',
       ],
       include_package_data = True,
   )
