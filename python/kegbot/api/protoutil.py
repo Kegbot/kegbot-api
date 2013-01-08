7cb358dc2240e01430dfe172a6970c5882221164
mike wakerly <opensource@hoho.com>
2013-01-07 23:27:38 -0800

    revert 020ad33

--- python/kegbot/api/protoutil.py
+++ python/kegbot/api/protoutil.py
@@ -20,7 +20,6 @@
 
 import datetime
 
-from google.protobuf.message import Message
 from kegbot.util import util
 
 try:
@@ -30,8 +29,6 @@ except ImportError:
   TIME_ZONE = 'America/Los_Angeles'
 
 def ProtoMessageToDict(message):
-  if not isinstance(message, Message):
-    message = ToProto(message, full=True)
   ret = util.AttrDict()
   #if not message.IsInitialized():
   #  raise ValueError, 'Message not initialized'
