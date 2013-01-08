4b3151e0495c7cd2b1907b8e55869e9e93bf108d
mike wakerly <opensource@hoho.com>
2013-01-07 23:28:02 -0800

    v0.1.6

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@
 For more information and documentation, see http://kegbot.org/docs
 """
 
-VERSION = '0.1.5'
+VERSION = '0.1.6'
 DOCLINES = __doc__.split('\n')
 SHORT_DESCRIPTION = DOCLINES[0]
 LONG_DESCRIPTION = '\n'.join(DOCLINES[2:])
